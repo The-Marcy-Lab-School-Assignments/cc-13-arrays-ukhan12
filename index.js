4332e286d46a57d0a57de004b39c0d2df44f8ca6
uzma <ubkhan3@gmail.com>
2021-11-09 00:07:27 +0000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+function average(numbers){
+    let avg = 0;
+    for(let i = 0; i < numbers.length; i++){
+        avg += numbers[i];
+    }
+    avg = avg/numbers.length;
+    return avg;
+}
+ 
+console.log(average([1, 2, 3, 4, 5]));
